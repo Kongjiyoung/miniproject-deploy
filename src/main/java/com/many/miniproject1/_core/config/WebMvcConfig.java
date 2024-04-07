57ad6ef4c0cd79ab9607b8af20f7239140998bbe
jiyoung <ij0512@naver.com>
2024-04-07 13:57:36 +0900

    배포준비완료

--- src/main/java/com/many/miniproject1/_core/config/WebMvcConfig.java
+++ src/main/java/com/many/miniproject1/_core/config/WebMvcConfig.java
@@ -16,15 +16,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //
 //    }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-
-        registry
-                .addResourceHandler("/images/**")
-                .addResourceLocations("file:./images/")
-                .setCachePeriod(60 * 60) // 초 단위 => 한시간
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver());
-    }
 }
